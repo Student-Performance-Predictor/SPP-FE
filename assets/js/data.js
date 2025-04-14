738faa7a199c17b3e2c1331dba4702367649ab87
tejas0629 <tejasupreti2964@gmail.com>
2025-04-15 00:33:39 +0530

    loader function

--- assets/js/data.js
+++ assets/js/data.js
@@ -88,3 +88,13 @@ const principals = [
         school: "Government Higher Secondary School"
     }
 ];
+
+function showLoader() {
+    document.getElementById("loader").style.display = "block";
+}
+
+function hideLoader() {
+    document.getElementById("loader").style.display = "none";
+}
+
+const BE_URL = "http://127.0.0.1:8000"
